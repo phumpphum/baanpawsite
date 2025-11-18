12b7f9decbf136483383335c010985b2f40f7c2e
phumpphum <phumchill@gmail.com>
2025-11-18 23:48:36 +0700

    Fix Django settings module path after project rename

--- baanpawsite/wsgi.py
+++ baanpawsite/wsgi.py
@@ -1,4 +1,4 @@
 import os
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shopsite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'baanpawsite.settings')
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import os, sys
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shopsite.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'baanpawsite.settings')
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
 if __name__ == '__main__':
