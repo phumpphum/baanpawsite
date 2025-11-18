27aba1b9b24058bc1d567d332c62af2c556bf872
phumpphum <phumchill@gmail.com>
2025-11-18 23:56:59 +0700

    Fix settings module + allowed hosts

--- baanpawsite/settings.py
+++ baanpawsite/settings.py
@@ -4,7 +4,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-change-me'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
